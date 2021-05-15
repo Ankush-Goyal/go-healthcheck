8b7a5472a59b1f37fbcf60ca6104d7f12acb95e4
Ankush Goyal <ankushgoyal030@gmail.com>
2021-05-15 20:59:06 +0530

    Added file checker and test file for registry and checker

--- pkg/checks/checks.go
+++ pkg/checks/checks.go
@@ -1,5 +1,10 @@
 package checks
 
+import (
+	"errors"
+	"os"
+)
+
 // Checker is the interface for a Health Checker
 type Checker interface {
 	// Check returns nil if the service is okay.
@@ -14,4 +19,15 @@ type CheckFunc func() error
 // to be passed as a Checker
 func (cf CheckFunc) Check() error {
 	return cf()
+}
+
+// FileChecker checks the existence of a file and returns an error
+// if the file exists.
+func FileChecker(f string) Checker {
+	return CheckFunc(func() error {
+		if _, err := os.Stat(f); err == nil {
+			return errors.New("file exists")
+		}
+		return nil
+	})
 }
